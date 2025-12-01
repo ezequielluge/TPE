6c431b4f35c3d77e75c55fc2ff221cddda30fdd2
Ezequiel <ezequielluge@gmail.com>
2025-12-01 14:20:42 -0300

    nada

--- main.c
+++ main.c
@@ -1,13 +1,8 @@
 #include <stdio.h>
 
-<<<<<<< HEAD
-int main(){
-    printf("LUGEEEEEEEEEEEEEEE");
-=======
 
 int main(void) {
     printf("Nueva linea");
     printf("Hello World!\n");
     return 0;
->>>>>>> refs/remotes/origin/main
 }
